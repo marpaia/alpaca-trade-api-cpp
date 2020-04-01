03d1e95cc879aa08f78fb8a8817ce69938c20403
Mike Arpaia <mike@arpaia.co>
2020-04-01 19:21:42 +0100

    Add serializable asset type

---
+++ alpaca/asset.cpp
@@ -0,0 +1,29 @@
+#include "alpaca/asset.h"
+
+#include "alpaca/json.h"
+#include "rapidjson/document.h"
+
+namespace alpaca {
+Status Asset::fromJSON(const std::string& json) {
+  rapidjson::Document d;
+  if (d.Parse(json.c_str()).HasParseError()) {
+    return Status(1, "Received parse error when deserializing asset JSON");
+  }
+
+  if (!d.IsObject()) {
+    return Status(1, "Deserialized valid JSON but it wasn't an asset object");
+  }
+
+  PARSE_STRING(asset_class, "class")
+  PARSE_BOOL(easy_to_borrow, "easy_to_borrow")
+  PARSE_STRING(exchange, "exchange")
+  PARSE_STRING(id, "id")
+  PARSE_BOOL(marginable, "marginable")
+  PARSE_BOOL(shortable, "shortable")
+  PARSE_STRING(status, "status")
+  PARSE_STRING(symbol, "symbol")
+  PARSE_BOOL(tradable, "tradable")
+
+  return Status();
+}
+} // namespace alpaca
---
+++ alpaca/asset.h
@@ -0,0 +1,34 @@
+#pragma once
+
+#include <string>
+
+#include "alpaca/status.h"
+
+namespace alpaca {
+
+/**
+ * @brief A type representing an Alpaca asset.
+ */
+class Asset {
+ public:
+  /**
+   * @brief A method for deserializing JSON into the current object state.
+   *
+   * @param json The JSON string
+   *
+   * @return a Status indicating the success or faliure of the operation.
+   */
+  Status fromJSON(const std::string& json);
+
+ public:
+  std::string asset_class;
+  bool easy_to_borrow;
+  std::string exchange;
+  std::string id;
+  bool marginable;
+  bool shortable;
+  std::string status;
+  std::string symbol;
+  bool tradable;
+};
+} // namespace alpaca

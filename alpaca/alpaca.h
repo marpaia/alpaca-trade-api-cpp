172d196698045d159c92b9eaca3b8140c7713e82
Mike Arpaia <mike@arpaia.co>
2020-04-03 17:39:06 +0100

    Add new headers to the aggregate header

--- alpaca/alpaca.h
+++ alpaca/alpaca.h
@@ -1,6 +1,12 @@
 #pragma once
 
 #include "alpaca/account.h"
+#include "alpaca/asset.h"
 #include "alpaca/client.h"
+#include "alpaca/clock.h"
 #include "alpaca/config.h"
+#include "alpaca/order.h"
+#include "alpaca/portfolio.h"
+#include "alpaca/position.h"
 #include "alpaca/status.h"
+#include "alpaca/watchlist.h"

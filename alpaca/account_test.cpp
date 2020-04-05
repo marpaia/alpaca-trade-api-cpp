939660128c66e806df81ee60c37a307155441dca
Mike Arpaia <mike@arpaia.co>
2020-04-05 15:28:39 +0100

    Add test for fromJSON methods on TradeActivity and NonTradeActivity

--- alpaca/account_test.cpp
+++ alpaca/account_test.cpp
@@ -44,6 +44,32 @@ const std::string kAccountConfigurationsJSON =
     "\"trade_confirm_email\": \"all\""
     "}";
 
+const std::string kTradeActivityJSON =
+    "{"
+    "\"activity_type\": \"FILL\","
+    "\"cum_qty\": \"1\","
+    "\"id\": \"20190524113406977::8efc7b9a-8b2b-4000-9955-d36e7db0df74\","
+    "\"leaves_qty\": \"0\","
+    "\"price\": \"1.63\","
+    "\"qty\": \"1\","
+    "\"side\": \"buy\","
+    "\"symbol\": \"LPCN\","
+    "\"transaction_time\": \"2019-05-24T15:34:06.977Z\","
+    "\"order_id\": \"904837e3-3b76-47ec-b432-046db621571b\","
+    "\"type\": \"fill\""
+    "}";
+
+const std::string kNonTradeActivityJSON =
+    "{"
+    "\"activity_type\": \"DIV\","
+    "\"id\": \"20190801011955195::5f596936-6f23-4cef-bdf1-3806aae57dbf\","
+    "\"date\": \"2019-08-01\","
+    "\"net_amount\": \"1.02\","
+    "\"symbol\": \"T\","
+    "\"qty\": \"2\","
+    "\"per_share_amount\": \"0.51\""
+    "}";
+
 TEST_F(AccountTest, testAccountConfigurationFromJSON) {
   alpaca::Account account;
   EXPECT_OK(account.fromJSON(kAccountsJSON));
@@ -51,7 +77,19 @@ TEST_F(AccountTest, testAccountConfigurationFromJSON) {
 }
 
 TEST_F(AccountTest, testAccountConfigurationsFromJSON) {
-    alpaca::AccountConfigurations account_configurations;
-    EXPECT_OK(account_configurations.fromJSON(kAccountConfigurationsJSON));
-    EXPECT_EQ(account_configurations.no_shorting, true);
+  alpaca::AccountConfigurations account_configurations;
+  EXPECT_OK(account_configurations.fromJSON(kAccountConfigurationsJSON));
+  EXPECT_EQ(account_configurations.no_shorting, true);
+}
+
+TEST_F(AccountTest, testTradeActivityFromJSON) {
+  alpaca::TradeActivity trade_activity;
+  EXPECT_OK(trade_activity.fromJSON(kTradeActivityJSON));
+  EXPECT_EQ(trade_activity.activity_type, "FILL");
+}
+
+TEST_F(AccountTest, testNonTradeActivityFromJSON) {
+  alpaca::NonTradeActivity non_trade_activity;
+  EXPECT_OK(non_trade_activity.fromJSON(kNonTradeActivityJSON));
+  EXPECT_EQ(non_trade_activity.activity_type, "DIV");
 }

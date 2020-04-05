1a0fa5fd2d6c2cdd1cde68c5d0dc15dc53a8e710
Mike Arpaia <mike@arpaia.co>
2020-04-05 15:54:04 +0100

    Add test for alpaca::Date::fromJSON

---
+++ alpaca/calendar_test.cpp
@@ -0,0 +1,22 @@
+#include "alpaca/calendar.h"
+
+#include "alpaca/testing.h"
+#include "glog/logging.h"
+#include "gtest/gtest.h"
+
+class CalendarTest : public ::testing::Test {};
+
+const std::string kDateJSON =
+  "{"
+  "\"date\": \"2018-01-03\","
+  "\"open\": \"09:30\","
+  "\"close\": \"16:00\""
+  "}";
+
+TEST_F(CalendarTest, testDateFromJSON) {
+  alpaca::Date date;
+  EXPECT_OK(date.fromJSON(kDateJSON));
+  EXPECT_EQ(date.open, "09:30");
+  EXPECT_EQ(date.close, "16:00");
+  EXPECT_EQ(date.date, "2018-01-03");
+}
